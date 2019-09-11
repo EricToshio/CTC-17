967aae79decc88e1ec1148624cec0886926d736b
EricToshio <eric_toshio@hotmail.com>
2019-09-10 23:11:37 -0300

    criando Chessboard

--- projeto-2/melhoria-iterativa/Chessboard.py
+++ projeto-2/melhoria-iterativa/Chessboard.py
@@ -1,3 +1,5 @@
+import random
+
 class Chessboard:
     def __init__(self, size):
         self.size = size
@@ -7,4 +9,24 @@ class Chessboard:
         self.board = []
         for _ in range(self.size):
             self.board.append([0]*self.size)
-        
+
+        # Coloca as rainhas em posicoes aleatorias
+        self.random()
+    
+    def random(self):
+        queens = 0
+        while queens != self.size:
+            x = random.randint(0, self.size - 1)
+            y = random.randint(0, self.size - 1)
+            if self.board[x][y] == 0:
+                self.board[x][y] = 1
+                queens +=1
+    
+    def next(self):
+        pass
+
+
+if __name__ == "__main__":
+    a = Chessboard(10)
+    print(a.board) 
+
