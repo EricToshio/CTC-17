7ea364e84bbc45999296b194384b241484b778c0
EricToshio <eric_toshio@hotmail.com>
2019-09-11 15:43:44 -0300

    n queens pronto

--- projeto-2/melhoria-iterativa/Chessboard.py
+++ projeto-2/melhoria-iterativa/Chessboard.py
@@ -1,4 +1,6 @@
 import random
+import math
+import copy
 
 class Chessboard:
     def __init__(self, size):
@@ -12,7 +14,7 @@ class Chessboard:
 
         # Coloca as rainhas em posicoes aleatorias
         self.random()
-    
+
     def random(self):
         queens = 0
         while queens != self.size:
@@ -21,12 +23,96 @@ class Chessboard:
             if self.board[x][y] == 0:
                 self.board[x][y] = 1
                 queens +=1
+
+
+    def next_boards(self, board):
+        # Faz um conjunto com as posicoes das rainhas
+        queens = set()
+        for x in range(self.size):
+            for y in range(self.size):
+                if board[x][y] == 1:
+                    queens.add((x,y))
+        # Encontra todos os possiveis proximos estados
+        # onde se muda a posicao de uma rainha somente
+        next_states = []
+        for queen in queens:
+            new_board = copy.deepcopy(board)
+            new_board[queen[0]][queen[1]] = 0
+
+            for x in range(self.size):
+                for y in range(self.size):
+                    if (x,y) not in queens:
+                        new_board_queen = copy.deepcopy(new_board)
+                        new_board_queen[x][y] = 1
+                        next_states.append(new_board_queen)
+        return next_states
+
+    def update_board(self, board):
+        self.board = board
+        self.queens = set()
+        for x in range(self.size):
+            for y in range(self.size):
+                if board[x][y] == 1:
+                    self.queens.add((x,y))
+
+    def evaluate(self, board):
+
+        cost = 0
+        queens = set()
+        for x in range(self.size):
+            for y in range(self.size):
+                if board[x][y] == 1:
+                    queens.add((x,y))
+        
+        for queen in queens:
+            cost += self.__evaluateIteration__(queen, [1,0], board)
+            cost += self.__evaluateIteration__(queen, [-1,0], board)
+            cost += self.__evaluateIteration__(queen, [0,1], board)
+            cost += self.__evaluateIteration__(queen, [0,-1], board)
+            cost += self.__evaluateIteration__(queen, [1,1], board)
+            cost += self.__evaluateIteration__(queen, [1,-1], board)
+            cost += self.__evaluateIteration__(queen, [-1,1], board)
+            cost += self.__evaluateIteration__(queen, [-1,-1], board)
+        return -cost
     
-    def next(self):
-        pass
+    def higest_next(self, actual_board):
+        nexts_boards = self.next_boards(actual_board)
+        best = None
+        value_best = -math.inf
+        for next_board in nexts_boards:
+            next_value = self.evaluate(next_board)
+            if next_value > value_best:
+                best = next_board
+                value_best = next_value
+        return best 
+    
+    def __evaluateIteration__(self, queen, add, board):
+        add_x = add[0]
+        add_y = add[1]
+        cost = 0
+        while queen[0]+add[0] >= 0 and queen[0]+add[0] < self.size and queen[1]+add[1] >= 0 and queen[1]+add[1] < self.size:
+            cost += board[queen[0]+add[0]][queen[1]+add[1]]
+            add[0]+=add_x
+            add[1]+=add_y
+        return cost
+
+        
 
 
 if __name__ == "__main__":
-    a = Chessboard(10)
-    print(a.board) 
+    a = Chessboard(5)
+    for i in range(a.size):
+        for j in range(a.size):
+            print(a.board[i][j], end=" ")
+        print() 
+
+    print(a.evaluate(a.board))
+    p = a.next_boards(a.board)[0]
+    p = [[0, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 1, 1], [1, 0, 0, 0, 0], [1, 0, 0, 0, 0]]
+    p = [[0, 1, 0, 0, 0], [0, 0, 0, 1, 1], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [0, 0, 1, 0, 0]]
+    for i in range(a.size):
+        for j in range(a.size):
+            print(p[i][j], end=" ")
+        print() 
+
 
--- projeto-2/melhoria-iterativa/hill-climbing.py
+++ projeto-2/melhoria-iterativa/hill-climbing.py
@@ -1,8 +1,22 @@
+from Chessboard import Chessboard
 class HillClimbing:
-    def __init__(self, n):
-        pass
-
+    def __init__(self, initial_node, higest_next, evaluate):
+        self.node = initial_node
+        self.higest_next = higest_next
+        self.evaluate = evaluate
+    
+    def find_best(self):
+        found  = False
+        while not found:
+            next = self.higest_next(self.node)
+            if self.evaluate(next) > self.evaluate(self.node):
+                self.node = next
+            else:
+                found = True
+        return self.node
 
 
 if __name__ == "__main__":
-    pass
+    board = Chessboard(5)
+    sol = HillClimbing(initial_node=board.board, higest_next=board.higest_next, evaluate=board.evaluate).find_best()
+    print(sol,board.evaluate(sol))
